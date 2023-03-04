827c95b3ce81bfe36fbbfdccc360391b950c2cca
smail <elgoufriismail1@gmail.com>
2023-03-04 19:15:35 +0100

    vehicule component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vehicules-front-end';
+  title = 'vehicules';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VehiculeComponent } from './vehicule/vehicule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiculeComponent
   ],
   imports: [
     BrowserModule
